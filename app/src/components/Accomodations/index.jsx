571802692ff67b1582ff1980550e67b8870d68d8
kotinat <natalia.meneghetti@paygo.com.br>
2020-06-21 20:02:58 -0300

    Adicionando Card e Paper aos componentes.

--- app/src/components/Accomodations/index.jsx
+++ app/src/components/Accomodations/index.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import Card from "../Card";
+
+const useStyles = makeStyles((theme) => ({
+  spacing: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+  },
+}));
 
 const Accomodations = () => {
- return (<h3>Accomodations</h3>)
-}
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid container className={classes.spacing}>
+        {[0, 1, 2, 3].map((value) => (
+          <Grid key={value} item>
+            <Card />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
 
 export default Accomodations;
--- app/src/components/Benefits/index.jsx
+++ app/src/components/Benefits/index.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      border: "#DDDDDD 1px solid",
+      margin: theme.spacing(1),
+      width: theme.spacing(24),
+      height: theme.spacing(48),
+    },
+  },
+  paragraph: {
+    fontSize: "14px",
+    padding: theme.spacing(1),
+    textAlign: "center",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
 const Benefits = () => {
- return (<h3>Benefits</h3>)
-}
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      {[0, 1, 2, 3].map((item) => (
+        <Paper className={classes.paragraph} elevation={0}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+            blanditiis tenetur unde suscipit, quam beatae rerum inventore
+            consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+            fugiat deleniti? Eum quasi quidem quibusdam.
+          </p>
+        </Paper>
+      ))}
+    </div>
+  );
+};
 
 export default Benefits;
