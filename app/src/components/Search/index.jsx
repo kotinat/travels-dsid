b98cd95081c87b1f3e114a44f93bbe3c05a75dc9
kotinat <natalia.meneghetti@paygo.com.br>
2020-06-21 17:10:17 -0300

    Ajustes Search, Grid e DatePicker

--- app/src/components/Search/index.jsx
+++ app/src/components/Search/index.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from '@material-ui/core/TextField';
+import DatePicker from "../DatePicker";
 
 const Search = () => {
- return (<div>
-   <input placeholder="Digite aqui a cidade do seu próximo destino." />
-   <label for=''>E a data?</label>
- </div>)
-}
+  return (
+    <div className="container-search">
+      <form>
+      <Grid container>
+        <Grid item xs={"12"}>
+          <TextField id='standard-basic' placeholder="Digite aqui a cidade do seu próximo destino." />
+        </Grid>
+        <Grid item xs={"3"}>
+          <DatePicker label="Entrada" />
+        </Grid>
+        <Grid item xs={"3"}>
+          <DatePicker label="Saída" />
+        </Grid>
+      </Grid>
+      </form>
+      
+    </div>
+  );
+};
 
 export default Search;
