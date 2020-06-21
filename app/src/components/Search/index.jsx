fdb681ed4b2473f8cd82b37bec0ed3e35e7f5449
kotinat <natalia.meneghetti@paygo.com.br>
2020-06-21 17:20:30 -0300

    Search básico finalizado, sem perfumaria.

--- app/src/components/Search/index.jsx
+++ app/src/components/Search/index.jsx
@@ -1,25 +1,51 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 import DatePicker from "../DatePicker";
 
+const useStyles = makeStyles((theme) => ({
+  inputs: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 const Search = () => {
+  const classes = useStyles();
+
   return (
     <div className="container-search">
       <form>
-      <Grid container>
-        <Grid item xs={"12"}>
-          <TextField id='standard-basic' placeholder="Digite aqui a cidade do seu próximo destino." />
+        <Grid container className={classes.inputs}>
+          <Grid item xs={"12"}>
+            <TextField
+              id="standard-basic"
+              placeholder="Digite aqui a cidade do seu próximo destino."
+              style={{ width: "50%" }}
+            />
+          </Grid>
+          <Grid item xs={"3"}>
+            <DatePicker label="Entrada" />
+          </Grid>
+          <Grid item xs={"3"}>
+            <DatePicker label="Saída" />
+          </Grid>
+          <Grid item xs={"12"}>
+            <TextField
+              id="standard-basic"
+              placeholder="Digite aqui a cidade do seu próximo destino."
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="secondary">
+              Buscar
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={"3"}>
-          <DatePicker label="Entrada" />
-        </Grid>
-        <Grid item xs={"3"}>
-          <DatePicker label="Saída" />
-        </Grid>
-      </Grid>
       </form>
-      
     </div>
   );
 };
