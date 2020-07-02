18889d201c56b6ef6e02e38e0223c0b3cfd9f049
Igor Araujo <igor.dearaujo@live.com>
2020-07-02 01:07:09 -0300

    date difference

--- app/src/controllers/dateAccomodations.js
+++ app/src/controllers/dateAccomodations.js
@@ -1,8 +1,36 @@
-import dateformat from 'dateformat'
+// import dateformat from 'dateformat'
+const dateformat = require('dateformat')
 
-export const transformDate = async (date) => {
-    date = new Date(date)
-    date = dateformat(date, "yyyy-mm-dd")
-    console.log(date)
-    return date
+const transformDate = async (date) => {
+    try{
+        date = new Date(date)
+        date = dateformat(date, "yyyy-mm-dd")
+        console.log(date)
+        return date
+    } catch (err) {
+        console.log(err)
+    }
+    
 }
+
+const testing = async () => {
+    var date1 = new Date('01/05/2020')
+
+    var date2 = new Date('01/20/2020')
+
+    var diffInTime = (date2.getTime() - date1.getTime())
+
+    var diffInDays = (diffInTime / (1000*3600*24))
+
+    console.log(diffInDays)
+
+    return diffInDays
+}
+
+async function test(){
+    var wtf = await testing()
+
+    console.log(wtf)
+    
+}
+test()
